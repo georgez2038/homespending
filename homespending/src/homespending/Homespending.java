951298e41cccd89a40da3ee9c6dcb3c2a9bb6065
george <george@DESKTOP-NLF4610.ht.home>
2022-05-24 10:54:06 -0400

    first commit

---
+++ homespending/src/homespending/Homespending.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package homespending;
+
+/**
+ * @author george
+ *
+ */
+public class Homespending {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world!\n");
+		System.out.print("This is first commit!");
+	}
+
+}
