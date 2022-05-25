5e8cc60ae59ce63a82e1fb1e6bbd5fa6d9fd4fe6
GitHub <michaelz9478@gmail.com>
2022-05-24 21:41:25 -0400

    5th commit

---
+++ homespending/src/homespending/Homespending1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package homespending;
+
+/**
+ * @author george
+ *
+ */
+public class Homespending1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world!\n");
+		System.out.println("This is second commit!");
+		System.out.println("This is third commit!");
+		System.out.println("This is forth commit!");
+		System.out.println("This is fifth commit!");
+	}
+
+}
